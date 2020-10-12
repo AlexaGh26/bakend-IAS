6596c4781c85b03310046caa9609bd17c51eaf99
Alexagh26 <alexagh26@gmail.com>
2020-10-12 11:47:21 -0500

    calculation of nighttime overtime difference

--- src/middleware/calculateHours.ts
+++ src/middleware/calculateHours.ts
@@ -5,47 +5,56 @@ import { servInformationModel } from '../models/servInformation';
 
 export const calculateHours = {
     calculate: (historyTechnical: any) => {
-        console.log('entro a calculate');
+
         const totalHoursDay = calculateHours.totalHours(historyTechnical);
-        const totalHoursExtraNight = calculateHours.calHoursExtraNight(historyTechnical);
+        const totalHoursExtraNight = calculateHours.HoursExtraNight(historyTechnical);
         return totalHoursDay;
     },
+
     totalHours: (historyTechnical: any) => {
-        console.log('entro a calcular total');
-        return historyTechnical.map((registry :any) => {
-            const {  dateInit, dateEnd } = registry
+
+        return historyTechnical.map((registry: any) => {
+            const { dateInit, dateEnd } = registry
             const dateInitFormat = moment(dateInit, 'DD/MM/YYYY HH:mm:ss');
-            const dateEndFormat = moment(dateEnd, 'DD/MM/YYYY HH:mm:ss');  
+            const dateEndFormat = moment(dateEnd, 'DD/MM/YYYY HH:mm:ss');
             const hoursWorked = dateEndFormat.diff(dateInitFormat, 'hours');
             return hoursWorked;
         })
     },
-     calHoursExtraNight: (historyTechnical:any) => {
-         console.log('entro a calHoursExtra nocturnas');
 
-         return historyTechnical.map((registry: any) => {
-             const { dateInit, dateEnd } = registry
-             const dateInitFormat = moment(dateInit, 'DD/MM/YYYY HH:mm:ss');
-             const dateEndFormat = moment(dateEnd, 'DD/MM/YYYY HH:mm:ss');
+    HoursExtraNight: (historyTechnical: any) => {
+        return historyTechnical.map((registry: any) => {
+            const { dateInit, dateEnd } = registry
 
+            const dateInitFormat = moment(dateInit, 'DD/MM/YYYY HH:mm:ss');
+            const dateEndFormat = moment(dateEnd, 'DD/MM/YYYY HH:mm:ss');
 
-             const hourLimitInit = moment(dateInit, 'DD/MM/YYYY HH:mm:ss');
-             const hourLimitEnd = moment(dateEnd, 'DD/MM/YYYY HH:mm:ss');
+            const hourLimitInit = moment(dateInit, 'DD/MM/YYYY HH:mm:ss');
+            const hourLimitEnd = moment(dateEnd, 'DD/MM/YYYY HH:mm:ss');
 
-             hourLimitInit.set({
-                 hour: '20',
-                 minute: '00',
-             })
-             hourLimitEnd.set({
+            hourLimitInit.set({
+                hour: '20',
+                minute: '00',
+            })
+            hourLimitEnd.set({
                 hour: '07',
                 minute: '00',
             })
-             //console.log(dateEndFormat.isAfter(hourLimitInit))
-             console.log(dateInitFormat.isBefore(hourLimitEnd));
-             
-             
-            })
-         }
+            const hoursAfter = dateEndFormat.isAfter(hourLimitInit);
+            const hourBefore = dateInitFormat.isBefore(hourLimitEnd);
+
+            if (hoursAfter) {
+                const totalDiff = dateEndFormat.diff(hourLimitInit, 'hours')
+                const minuteDiff = (dateEndFormat.diff(hourLimitInit, 'minutes') - (totalDiff * 60))
+                //console.log(totalDiff, minuteDiff);
+            }
+            if (hourBefore) {
+                const totalDiff = dateInitFormat.diff(hourLimitEnd, 'hours')
+                const minuteDiff = (dateInitFormat.diff(hourLimitEnd, 'minutes') - (totalDiff * 60))
+                console.log(totalDiff, minuteDiff);
+            }
+        })
+    }
 
 };
 
