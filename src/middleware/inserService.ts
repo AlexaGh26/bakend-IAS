6f4ea3cfc8d2e2207367e7a7d0c587152864a3db
Alexagh26 <alexagh26@gmail.com>
2020-10-13 00:29:25 -0500

    change configuration body insertNewService

--- src/middleware/inserService.ts
+++ src/middleware/inserService.ts
@@ -1,11 +1,23 @@
 const fs = require('fs');
 
+import moment from 'moment';
 import { servInformationModel } from '../models/servInformation';
 
 export const save = {
     saveNewService: async (req: any, res: any) => {
+        const dateFormat = 'DD/MM/YYYY HH:mm:ss';
+        const {dateInit, hoursInit, dateEnd, hoursEnd, registrationNumber, typeService} = req.body;
+
         const db: Array<object> = JSON.parse(fs.readFileSync('./database.json', 'utf8'));
-        let body: servInformationModel = req.body;
+        
+        const dateInitialSumHour = moment(dateInit, dateFormat).add(hoursInit, 'hours').format(dateFormat);
+        const dateFinalSumHour = moment(dateInit, dateFormat).add(hoursInit, 'hours').format(dateFormat);
+        let body: servInformationModel = {
+            idTechnical: registrationNumber,
+            dateInit: dateInitialSumHour,
+            dateEnd: dateFinalSumHour,
+            typeService : typeService,
+        };
         db.push(body)
         let json = JSON.stringify(db);
         fs.writeFileSync('./database.json', json, 'utf8');
